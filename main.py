94b975efb7104281f760dac328cd1f576ceb20b3
Klimxxx <132575216+Klimxxx@users.noreply.github.com>
2023-05-22 01:07:02 +0500

    add params

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-
-response = requests.get("https://playground.learnqa.ru/api/get_text")
+payload = {"name": "user"}
+response = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
 
 print(response.text)
