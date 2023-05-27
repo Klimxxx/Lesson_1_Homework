f774d8e6554695d93b9db421761f17b9cfd22c54
Klimxxx <132575216+Klimxxx@users.noreply.github.com>
2023-05-27 17:30:46 +0500

    lesson parcing JSON 2

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from json.decoder import JSONDecodeError
 import requests
 
 payload = {"name": "user"}
 response = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
-print(response.text)
+try:
+    parsed_response_text = response.json()
+    print(parsed_response_text["answer"])
+except JSONDecodeError:
+    print("Response is not a JSON format ")
