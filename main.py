a53facba8c3008d0b23b8d08115a575db1da0236
Klimxxx <132575216+Klimxxx@users.noreply.github.com>
2023-05-27 20:05:51 +0500

    lesson codes of anwers

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import requests
 
 response = requests.get("https://playground.learnqa.ru/api/check_type", params={"param1":"value1"})
+print(response.status_code)
+
+response = requests.get("https://playground.learnqa.ru/api/get_500")
+print(response.status_code)
 print(response.text)
-response2 = requests.post("https://playground.learnqa.ru/api/check_type", data={"param2":"value2"})
-print(response2.text)
+
+response = requests.get("https://playground.learnqa.ru/api/something")
+print(response.status_code)
+print(response.text)
+
+response = requests.post("https://playground.learnqa.ru/api/get_301", allow_redirects = False)
+print(response.text)
+print("если поменяю на True то будет - 403 а пока 301 - ", response.status_code)
+
+response = requests.post("http://playground.learnqa.ru/api/get_301", allow_redirects=True)
+first_response = response.history[0]
+second_response = response
+
+print(first_response.url)
+print(second_response.url)
