afe066deb9d979eca38a7dc0d8c325a57914622d
Klimxxx <132575216+Klimxxx@users.noreply.github.com>
2023-05-29 09:39:13 +0500

    lesson 11 Cookies v.2

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ response1 = requests.post("https://playground.learnqa.ru/api/get_auth_cookie", d
 cookie_value = response1.cookies.get('auth_cookie')
 cookies = {}
 if cookie_value is not None:
-    cookie.update{'auth_cookie': cookie_value}
+    cookies.update({'auth_cookie': cookie_value})
 response2 = requests.post("https://playground.learnqa.ru/api/check_auth_cookie", cookies = cookies)
 print (response2.text)
 
